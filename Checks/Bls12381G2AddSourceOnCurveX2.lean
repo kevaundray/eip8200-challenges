42ec047aa12452088e82b3e94b2f557281c3d79b
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2026-08-12 03:13:39 +0100

    proof(g2add): refine on-curve x square

---
+++ Checks/Bls12381G2AddSourceOnCurveX2.lean
@@ -0,0 +1,6 @@
+import Challenge.Bls12381G2Add.Reference.Proofs.SourceOnCurveX2
+
+open Challenge.Bls12381G2Add.Reference.Proofs.SourceSemantics
+
+#check onCurveX2_canonical
+#check onCurveX2_toLawful
