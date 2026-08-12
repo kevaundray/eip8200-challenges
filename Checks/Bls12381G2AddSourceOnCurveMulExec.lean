d21cc8d89d8bb4da85b64672150b263b56e01360
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2026-08-12 02:51:16 +0100

    proof(g2add): execute on-curve products

---
+++ Checks/Bls12381G2AddSourceOnCurveMulExec.lean
@@ -0,0 +1,7 @@
+import Challenge.Bls12381G2Add.Reference.Proofs.SourceOnCurveMulExec
+
+open Challenge.Bls12381G2Add.Reference.Proofs.SourceSemantics
+
+#check exec_onCurveStmt0
+#check exec_onCurveStmt1
+#check exec_onCurveStmt2
