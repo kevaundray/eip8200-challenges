4840ed7d39363cc7492e34d3e863196a904eedfc
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2026-08-12 06:36:01 +0100

    prove complete G2ADD source specification

---
+++ Checks/Bls12381G2AddSourceSpec.lean
@@ -0,0 +1,7 @@
+import Challenge.Bls12381G2Add.Reference.Proofs.SourceSpec
+
+set_option warningAsError true
+
+open Challenge.Bls12381G2Add.Reference.Proofs.SourceSemantics
+
+#check run_matches_spec
