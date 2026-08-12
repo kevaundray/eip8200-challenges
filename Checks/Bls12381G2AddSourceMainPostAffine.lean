0b3e570be2f7ae12965365a07e0c94be9bddcf5a
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2026-08-12 05:22:16 +0100

    connect G2ADD postlude to affine addition

---
+++ Checks/Bls12381G2AddSourceMainPostAffine.lean
@@ -0,0 +1,8 @@
+import Challenge.Bls12381G2Add.Reference.Proofs.SourceMainPostAffine
+
+open Challenge.Bls12381G2Add.Reference.Proofs.SourceSemantics
+
+#check mainPostPoint
+#check mainPostCoordinates_canonical
+#check mainPostPoint_eq_add_of_x_ne
+#check mainPostPoint_eq_double
