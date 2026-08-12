bb2cf710f9b568b62dff2fe0cccee5f63a05d042
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2026-08-12 03:42:41 +0100

    bls12-381 g2add: execute point output helpers

---
+++ Checks/Bls12381G2AddSourcePointStoresExec.lean
@@ -0,0 +1,7 @@
+import Challenge.Bls12381G2Add.Reference.Proofs.SourcePointStoresExec
+
+open Challenge.Bls12381G2Add.Reference.Proofs.SourceSemantics
+
+#check eval_clearPoint
+#check eval_copyPoint
+#check eval_storePoint
