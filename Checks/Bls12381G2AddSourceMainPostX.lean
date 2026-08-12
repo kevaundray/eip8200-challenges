e7c493531ad4f86048612443d559b260644f95b3
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2026-08-12 05:17:34 +0100

    prove G2ADD postlude x coordinate

---
+++ Checks/Bls12381G2AddSourceMainPostX.lean
@@ -0,0 +1,9 @@
+import Challenge.Bls12381G2Add.Reference.Proofs.SourceMainPostX
+
+open Challenge.Bls12381G2Add.Reference.Proofs.SourceSemantics
+
+#check mainPostState0_canonical
+#check mainPostState0_toLawful
+#check mainPostState2_fp2At_low
+#check mainPostState2_canonical
+#check mainPostState2_toLawful
