6f4339de1bf93e66ab833b6421ca49daf4637387
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2026-08-12 03:26:39 +0100

    proof(g2add): recover on-curve twist memory

---
+++ Checks/Bls12381G2AddSourceOnCurveConstantMemory.lean
@@ -0,0 +1,6 @@
+import Challenge.Bls12381G2Add.Reference.Proofs.SourceOnCurveConstantMemory
+
+open Challenge.Bls12381G2Add.Reference.Proofs.SourceSemantics
+
+#check onCurveConstant_value
+#check onCurveConstant_x3
