749706782111e259c12fde83c32b83a2743f775a
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2026-08-12 06:25:13 +0100

    compose G2ADD unequal source path

---
+++ Checks/Bls12381G2AddSourceSpecFiniteUnequal.lean
@@ -0,0 +1,7 @@
+import Challenge.Bls12381G2Add.Reference.Proofs.SourceSpecFiniteUnequal
+
+set_option warningAsError true
+
+open Challenge.Bls12381G2Add.Reference.Proofs.SourceSemantics
+
+#check run_main_unequal_matches_add
