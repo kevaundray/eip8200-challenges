f29993d39bd6e98b325056fa9031fce35eb329e9
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2026-08-12 06:30:08 +0100

    compose G2ADD valid source dispatcher

---
+++ Checks/Bls12381G2AddSourceSpecValid.lean
@@ -0,0 +1,7 @@
+import Challenge.Bls12381G2Add.Reference.Proofs.SourceSpecValid
+
+set_option warningAsError true
+
+open Challenge.Bls12381G2Add.Reference.Proofs.SourceSemantics
+
+#check run_valid_matches_decoded
