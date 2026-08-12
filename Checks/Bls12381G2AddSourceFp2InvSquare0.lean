6f07f3b726f7b9a337b7aa3d184b80e0b7c5c904
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2026-08-12 02:18:11 +0100

    proof(g2add): execute first Fp2 inversion square

---
+++ Checks/Bls12381G2AddSourceFp2InvSquare0.lean
@@ -0,0 +1,6 @@
+import Challenge.Bls12381G2Add.Reference.Proofs.SourceFp2InvSquare0
+
+open Challenge.Bls12381G2Add.Reference.Proofs.SourceSemantics
+
+#check exec_fp2InvStmt0
+#check exec_fp2InvSquare0Stores
