a391799115f528c606fd697dcdd37c5e167e17cd
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2026-08-12 03:36:13 +0100

    bls12-381 g2add: define point predicates

---
+++ Checks/Bls12381G2AddSourcePointPredicatesDefs.lean
@@ -0,0 +1,10 @@
+import Challenge.Bls12381G2Add.Reference.Proofs.SourcePointPredicatesDefs
+
+open Challenge.Bls12381G2Add.Reference.Proofs.SourceSemantics
+
+#check pointPaddingZeroValue
+#check pointValidValue
+#check pointZeroValue
+#check lookup_pointPaddingZero
+#check lookup_pointValid
+#check lookup_pointZero
