8bf0d0cf4be0a078be358a20b2017bf4014a1c9d
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2026-08-12 03:23:54 +0100

    proof(g2add): certify on-curve twist constant

---
+++ Checks/Bls12381G2AddSourceOnCurveConstant.lean
@@ -0,0 +1,6 @@
+import Challenge.Bls12381G2Add.Reference.Proofs.SourceOnCurveConstant
+
+open Challenge.Bls12381G2Add.Reference.Proofs.SourceSemantics
+
+#check onCurveTwistB_canonical
+#check onCurveTwistB_toLawful
