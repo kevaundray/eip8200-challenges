401dbdc30c5ce2ea26dd0510072d8a4f6cf1dbd2
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2026-08-12 06:26:49 +0100

    compose G2ADD doubling source path

---
+++ Checks/Bls12381G2AddSourceSpecFiniteDouble.lean
@@ -0,0 +1,7 @@
+import Challenge.Bls12381G2Add.Reference.Proofs.SourceSpecFiniteDouble
+
+set_option warningAsError true
+
+open Challenge.Bls12381G2Add.Reference.Proofs.SourceSemantics
+
+#check run_main_double_matches_add
