324f6ad3391e1139853a7a04c50de69997649943
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2026-08-12 02:21:33 +0100

    proof(g2add): execute Fp2 inversion real component

---
+++ Checks/Bls12381G2AddSourceFp2InvReal.lean
@@ -0,0 +1,6 @@
+import Challenge.Bls12381G2Add.Reference.Proofs.SourceFp2InvReal
+
+open Challenge.Bls12381G2Add.Reference.Proofs.SourceSemantics
+
+#check exec_fp2InvStmt10
+#check exec_fp2InvRealStores
