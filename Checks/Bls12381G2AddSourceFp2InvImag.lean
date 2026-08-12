a28340f99978b95001b7e4e45c946f13f2e34855
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2026-08-12 02:23:26 +0100

    proof(g2add): execute Fp2 inversion imaginary component

---
+++ Checks/Bls12381G2AddSourceFp2InvImag.lean
@@ -0,0 +1,6 @@
+import Challenge.Bls12381G2Add.Reference.Proofs.SourceFp2InvImag
+
+open Challenge.Bls12381G2Add.Reference.Proofs.SourceSemantics
+
+#check exec_fp2InvStmt14
+#check exec_fp2InvImagStores
