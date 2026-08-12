e4e468635fc527ea8d333662be5aad9e3bf39445
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2026-08-12 02:20:41 +0100

    proof(g2add): execute Fp2 norm inversion

---
+++ Checks/Bls12381G2AddSourceFp2InvScalar.lean
@@ -0,0 +1,6 @@
+import Challenge.Bls12381G2Add.Reference.Proofs.SourceFp2InvScalar
+
+open Challenge.Bls12381G2Add.Reference.Proofs.SourceSemantics
+
+#check exec_fp2InvStmt7
+#check exec_fp2InvScalarStores
