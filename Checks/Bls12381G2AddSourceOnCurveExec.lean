c260d0225aabc9ada1f9a86b98cd2b95de1ed58d
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2026-08-12 02:57:51 +0100

    proof(g2add): compose on-curve execution

---
+++ Checks/Bls12381G2AddSourceOnCurveExec.lean
@@ -0,0 +1,7 @@
+import Challenge.Bls12381G2Add.Reference.Proofs.SourceOnCurveExec
+
+open Challenge.Bls12381G2Add.Reference.Proofs.SourceSemantics
+
+#check step_onCurveBody
+#check onCurveBodyResultEnv_yes
+#check step_onCurve
